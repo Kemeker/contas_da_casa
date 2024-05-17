b3d0e05760d06b17a9db6ef9ef13a79ecf223599
Kemeker <kemeker@gmail.com>
2024-05-17 14:01:16 -0300

    Feat: realizado ajustes no botao do forms, ele agora esta funcionando corretamente

--- Finaças/finance/components/Forms.js
+++ Finaças/finance/components/Forms.js
@@ -1,85 +1,87 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, Image, TouchableOpacity, BackHandler } from 'react-native';
+import Adicionar from '../assets/adicionar.png';
 
 export default function Forms() {
   const [nome, setNome] = useState('');
   const [salario, setSalario] = useState('');
-  const [isFormVisible, setIsFormVisible] = useState(false); // Estado para controlar a visibilidade do formulário
+  const [formularioVisivel, setFormularioVisivel] = useState(false); // Estado para controlar a visibilidade do formulário
 
   const handleSubmit = () => {
     alert(`Nome: ${nome}, Salário: ${salario} Cadastrado com sucesso...`);
-    setIsFormVisible(false); // Esconde o formulário após o envio
+    setFormularioVisivel(false); // Esconde o formulário após o envio
   };
 
+  useEffect(() => {
+    const voltarAcaoDoBotaoAdicionar = () => {
+      if (formularioVisivel) {
+        setFormularioVisivel(false); // Esconde o formulário se o botão voltar for pressionado
+        return true; // Impede o comportamento padrão do botão voltar
+      }
+      return false; // Permite o comportamento padrão do botão
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', voltarAcaoDoBotaoAdicionar);
+
+    return () => backHandler.remove();
+  }, [formularioVisivel]);
+
   return (
-    
-        <View style={styles.container} >
-            <TouchableOpacity onPress={()=> setIsFormVisible(true) }>
-                <Image
-                    source={{uri: 'https://cdn-icons-png.flaticon.com/256/1004/1004733.png' }}
-                    style={{ width: 60, height: 60 }}
-                />    
-            </TouchableOpacity>
-            {isFormVisible && (
-            
-                <View style={styles.formContainer}>
-                    <Text style={styles.label}>Nome:</Text>
-                        <TextInput
-                            style={styles.input}
-                            value={nome}
-                            onChangeText={setNome}
-                            placeholder="Digite seu nome"
-                        />
-                    <Text style={styles.label}>Salário:</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={salario}
-                        onChangeText={setSalario}
-                        placeholder="Digite seu salário"
-                        keyboardType="numeric"
-                    />
-                    <Button
-                        style={styles.button}
-                        title="Enviar"
-                        onPress={handleSubmit}
-                    />
-                </View>
-            )}
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setFormularioVisivel(true)}>
+        <Image
+          source={Adicionar}
+          style={{ width: 60, height: 60 }}
+        />
+      </TouchableOpacity>
+      {formularioVisivel && (
+        <View style={styles.formContainer}>
+          <Text style={styles.label}>Nome:</Text>
+          <TextInput
+            style={styles.input}
+            value={nome}
+            onChangeText={setNome}
+            placeholder="Digite seu nome"
+          />
+          <Text style={styles.label}>Salário:</Text>
+          <TextInput
+            style={styles.input}
+            value={salario}
+            onChangeText={setSalario}
+            placeholder="Digite seu salário"
+            keyboardType="numeric"
+          />
+          <Button
+            title="Enviar"
+            onPress={handleSubmit}
+          />
         </View>
-    
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
-      backgroundColor: 'lightblue',
-     
-    },
-    formContainer: {
-        marginTop: 15,
-        width: '100%',
-        
-      
-    },
-    input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginBottom: 20,
-      paddingHorizontal: 10,
-      
-    },
-    label: {
-      marginBottom: 3,
-      color: 'black', 
-    },
-    button: {
-        backgroundColor: 'balck',
-    },
-    
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'lightblue',
+  },
+  formContainer: {
+    marginTop: 15,
+    width: '100%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  label: {
+    marginBottom: 3,
+    color: 'black',
+  },
 });
-  
